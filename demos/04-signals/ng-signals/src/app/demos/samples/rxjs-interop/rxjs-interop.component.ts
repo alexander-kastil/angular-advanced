a24659afb0387deec2486cc47f6b4cc1825fd254
Alexander Kastil <alexander.kastil@integrations.at>
2024-09-14 14:46:51 +0200

    feat: writable signal

--- demos/04-signals/ng-signals/src/app/demos/samples/rxjs-interop/rxjs-interop.component.ts
+++ demos/04-signals/ng-signals/src/app/demos/samples/rxjs-interop/rxjs-interop.component.ts
@@ -1,5 +1,6 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { computeMsgId } from '@angular/compiler';
+import { Component, computed, effect, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { of, startWith } from 'rxjs';
 import { BorderDirective, CenteredDirective } from 'src/app/shared/formatting/formatting-directives';
@@ -15,4 +16,16 @@ import { MarkdownRendererModule } from 'src/app/shared/markdown-renderer/markdow
 export class RxjsInteropComponent {
   amount$ = of(10).pipe(startWith(0));
   amount = toSignal(this.amount$, { initialValue: 0 });
+  writeableAmount = signal(0);
+
+  // creates a writeable signal that is bound to the amount signal
+  // an alternative could to subscribe the observable and create a writeable signal
+  createWriteAmount = effect(() => {
+    this.writeableAmount.set(this.amount());
+  });
+
+  updateAmount() {
+    // this.amount.set(this.writeAmount());
+    this.writeableAmount.update(curr => curr + 1);
+  }
 }
