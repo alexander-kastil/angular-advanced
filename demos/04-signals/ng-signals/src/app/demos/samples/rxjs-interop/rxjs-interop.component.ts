441ee39b1595b42f1b6b1cc2ede413ec6d646ac7
Alexander Kastil <alexander.kastil@integrations.at>
2024-09-14 14:58:03 +0200

    rxjs-interop

--- demos/04-signals/ng-signals/src/app/demos/samples/rxjs-interop/rxjs-interop.component.ts
+++ demos/04-signals/ng-signals/src/app/demos/samples/rxjs-interop/rxjs-interop.component.ts
@@ -1,22 +1,28 @@
 import { AsyncPipe } from '@angular/common';
-import { computeMsgId } from '@angular/compiler';
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { of, startWith } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { of } from 'rxjs';
 import { BorderDirective, CenteredDirective } from 'src/app/shared/formatting/formatting-directives';
 import { MarkdownRendererModule } from 'src/app/shared/markdown-renderer/markdown-renderer.module';
 
 @Component({
   selector: 'app-rxjs-interop',
   standalone: true,
-  imports: [AsyncPipe, MarkdownRendererModule, BorderDirective, CenteredDirective],
+  imports: [
+    AsyncPipe,
+    MatButtonModule,
+    MarkdownRendererModule,
+    BorderDirective,
+    CenteredDirective
+  ],
   templateUrl: './rxjs-interop.component.html',
   styleUrl: './rxjs-interop.component.scss'
 })
 export class RxjsInteropComponent {
-  amount$ = of(10).pipe(startWith(0));
-  amount = toSignal(this.amount$, { initialValue: 0 });
-  writeableAmount = signal(0);
+  amount$ = of(10);
+  amount = toSignal(this.amount$, { initialValue: 10 });
+  writeableAmount = signal(this.amount());
 
   // creates a writeable signal that is bound to the amount signal
   // an alternative could to subscribe the observable and create a writeable signal
