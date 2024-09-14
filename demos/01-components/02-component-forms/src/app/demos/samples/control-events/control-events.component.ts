d981b439cb1968cf0344776949f0c8e030e8882e
Alexander Kastil <alexander.kastil@integrations.at>
2024-09-14 08:25:01 +0200

    theme fix

--- demos/01-components/02-component-forms/src/app/demos/samples/control-events/control-events.component.ts
+++ demos/01-components/02-component-forms/src/app/demos/samples/control-events/control-events.component.ts
@@ -1,9 +1,9 @@
-import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ControlEvent, FormControl, PristineChangeEvent, ReactiveFormsModule, StatusChangeEvent, TouchedChangeEvent, Validators, ValueChangeEvent } from '@angular/forms';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MarkdownRendererComponent } from 'src/app/shared/markdown-renderer/markdown-renderer.component';
+import { BoxedDirective } from 'src/app/shared/ux-lib/formatting/formatting-directives';
 
 @Component({
   selector: 'app-control-events',
@@ -13,8 +13,8 @@ import { MarkdownRendererComponent } from 'src/app/shared/markdown-renderer/mark
     MatLabel,
     MatInput,
     ReactiveFormsModule,
-    JsonPipe,
-    MarkdownRendererComponent
+    MarkdownRendererComponent,
+    BoxedDirective
   ],
   templateUrl: './control-events.component.html',
   styleUrl: './control-events.component.scss'
