b6257483258990eb32644ca0f78b635d53a23acd
Alexander Kastil <alexander.kastil@integrations.at>
2024-10-28 10:38:03 +0100

    Refactor error handler to functional implementation (#449)

--- demos/05-routing-app-init/routing-app-init/src/app/error/error.handler.ts
+++ demos/05-routing-app-init/routing-app-init/src/app/error/error.handler.ts
@@ -1,19 +1,12 @@
-import { Injectable, Injector, ErrorHandler, inject } from '@angular/core';
-import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class GlobalErrorHandler implements ErrorHandler {
-  injector = inject(Injector);
-
-  handleError(error: Error | HttpErrorResponse) {
-    const router = this.injector.get(Router);
-    console.warn('An error occurred:', error);
-    if (error.message) {
-      console.warn('Err Message:', error.message);
-    }
-    router.navigate(['/error'], { state: { data: (error as Error).message } });
+export function globalErrorHandler(error: Error | HttpErrorResponse) {
+  const router = inject(Router);
+  console.warn('An error occurred:', error);
+  if (error.message) {
+    console.warn('Err Message:', error.message);
   }
+  router.navigate(['/error'], { state: { data: (error as Error).message } });
 }
