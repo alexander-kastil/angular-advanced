0fc9d66354fbbe7f310574d921b801df3663ac64
Alexander Kastil <alexander.kastil@integrations.at>
2024-11-10 09:44:09 +0100

    Add Deep Signals component with pagination and update routes; include markdown documentation

--- demos/03-signals/ng-signals/src/app/demos/demo.routes.ts
+++ demos/03-signals/ng-signals/src/app/demos/demo.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { DemoContainerComponent } from './demo-container/demo-container.component';
 import { AsyncRequestsComponent } from './samples/async-requests/async-requests.component';
 import { ContainerPresenterSignalsComponent } from './samples/container-presenter/container-presenter-signals.component';
+import { DeepSignalsComponent } from './samples/deep-signals/deep-signals.component';
+import { LinkedSignalComponent } from './samples/linked-signal/linked-signal.component';
 import { ModelInputsComponent } from './samples/model-inputs/model-inputs.component';
 import { RxjsInteropComponent } from './samples/rxjs-interop/rxjs-interop.component';
 import { SignalEffectsComponent } from './samples/signal-effects/signal-effects.component';
@@ -9,7 +11,6 @@ import { SignalInputsComponent } from './samples/signal-inputs/signal-inputs.com
 import { SignalsBasicsComponent } from './samples/signals-basics/signals-basics.component';
 import { SignalsEventBusComponent } from './samples/signals-event-bus/signals-event-bus.component';
 import { ZonelessChangeDetectionComponent } from './samples/zoneless-change-detection/zoneless-change-detection.component';
-import { LinkedSignalComponent } from './samples/linked-signal/linked-signal.component';
 
 export const demoRoutes: Routes = [
   {
@@ -19,6 +20,7 @@ export const demoRoutes: Routes = [
       { path: 'signals-basics', component: SignalsBasicsComponent },
       { path: 'signals-effects', component: SignalEffectsComponent },
       { path: 'async-requests', component: AsyncRequestsComponent },
+      { path: 'deep-signals', component: DeepSignalsComponent },
       { path: 'linked-signals', component: LinkedSignalComponent },
       { path: 'rxjs-interop', component: RxjsInteropComponent },
       { path: 'signal-inputs', component: SignalInputsComponent },
--- demos/03-signals/ng-signals/src/app/demos/samples/deep-signals/deep-signals.component.ts
+++ demos/03-signals/ng-signals/src/app/demos/samples/deep-signals/deep-signals.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { deepComputed } from '@ngrx/signals';
 import { MarkdownRendererComponent } from '../../../shared/markdown-renderer/markdown-renderer.component';
+import { customersRoutes } from '../../../customers/customer.routes';
+import { MatButton } from '@angular/material/button';
+import { JsonPipe } from '@angular/common';
 
 @Component({
     selector: 'app-deep-signals',
-    imports: [MarkdownRendererComponent],
+    standalone: true,
+    imports: [
+        MarkdownRendererComponent,
+        MatButton,
+        JsonPipe
+    ],
     templateUrl: './deep-signals.component.html',
-    styleUrl: './deep-signals.component.scss'
+    styleUrls: ['./deep-signals.component.scss']
 })
 export class DeepSignalsComponent {
+    limit = signal(10);
+    current = signal(0);
+    totalItems = signal(100);
 
+    pagination = deepComputed(() => ({
+        currentPage: this.current(),
+        pageSize: this.limit(),
+        totalPages: Math.ceil(this.totalItems() / this.limit()),
+    }));
+
+    change = effect(() => {
+        console.log('Pagination changed', this.pagination());
+    });
+
+    moveTo(move: number) {
+        this.current.update((curr) => {
+            const newValue = curr + move;
+            if (newValue < 0 || newValue >= this.limit()) {
+                console.log("not supported");
+                return curr;
+            }
+            return newValue;
+        });
+    }
 }
