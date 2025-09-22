2155f384e8b35a602ec40daa372cc3a29cba234b
Alexander Kastil <alexander.kastil@integrations.at>
2025-09-22 14:23:55 +0200

    refactor: improve formatting and logging in AlertComponent and ViewChildComponent
    - Updated console log message in AlertComponent from 'Alert' to 'Logging Alert' for clarity.
    - Reformatted code in alert.component.ts and view-child.component.ts for consistent indentation.
    - Adjusted HTML structure in view-child.component.html for better readability.
    - Ensured markdown file view-queries.md has consistent casing in the first line.

--- demos/01-components/02-component-forms/ng-components-forms/src/app/demos/samples/view-child/alert/alert.component.ts
+++ demos/01-components/02-component-forms/ng-components-forms/src/app/demos/samples/view-child/alert/alert.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-alert',
-    imports: [],
-    templateUrl: './alert.component.html',
-    styleUrl: './alert.component.scss'
+  selector: 'app-alert',
+  imports: [],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.scss'
 })
 export class AlertComponent {
   logAlert() {
-    console.log('Alert');
+    console.log('Logging Alert');
   }
 }
--- demos/01-components/02-component-forms/ng-components-forms/src/app/demos/samples/view-child/view-child.component.ts
+++ demos/01-components/02-component-forms/ng-components-forms/src/app/demos/samples/view-child/view-child.component.ts
@@ -10,18 +10,18 @@ import { AlertComponent } from './alert/alert.component';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-    selector: 'app-view-child',
-    templateUrl: './view-child.component.html',
-    styleUrls: ['./view-child.component.scss'],
-    imports: [
-        MarkdownRendererComponent,
-        MatCard,
-        MatCardHeader,
-        MatCardTitle,
-        MatCardContent,
-        MatButtonModule,
-        AlertComponent
-    ]
+  selector: 'app-view-child',
+  templateUrl: './view-child.component.html',
+  styleUrls: ['./view-child.component.scss'],
+  imports: [
+    MarkdownRendererComponent,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatButtonModule,
+    AlertComponent
+  ]
 })
 export class ViewChildComponent {
   nbrLiters = viewChild.required('liters', { read: ElementRef });
@@ -40,8 +40,7 @@ export class ViewChildComponent {
     }
   }
 
-  hideAlert() {
+  logAlert() {
     this.alert().logAlert();
-    // this.alert().nativeElement.style.display = 'none';
   }
 }
