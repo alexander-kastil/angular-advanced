6a74dc6983f2e5db5ed7933d3afe8ecab1a008c4
Alexander Kastil <alexander.kastil@integrations.at>
2025-09-24 14:53:48 +0200

    refactor: enhance PhonenumberPipe error handling and testing
    - Updated PhonenumberPipe to throw an error when input is undefined or null.
    - Improved test cases in phonenumber.pipe.spec.ts to verify error handling for undefined and null inputs.
    - Ensured that the pipe correctly handles and formats valid phone numbers.

--- demos/06-testing/jasmine-testing/src/app/demos/samples/pipe/phonenumber.pipe.ts
+++ demos/06-testing/jasmine-testing/src/app/demos/samples/pipe/phonenumber.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'phonenumber',
-    standalone: true,
+  name: 'phonenumber',
+  standalone: true,
 })
 export class PhonenumberPipe implements PipeTransform {
   transform(value: any, args?: any): any {
-    if (undefined !== value && value.length === 10) {
+    if (value === undefined || value === null) {
+      throw new Error('No input provided to phone pipe');
+    }
+    if (value.length === 10) {
       return (
         `(${value.substring(0, 3)}) ${value.substring(3, 6)} ${value.substring(6)}`
       );
